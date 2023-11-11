e117a14ec8f462ddc493dca47f70862c75677f06
YOO-uN-ee <jiyoonp0228@gmail.com>
2023-11-11 12:51:42 -0600

    TASK1&TASK2 basic completed

--- src/minelink/preprocess.py
+++ src/minelink/preprocess.py
@@ -50,23 +50,26 @@ def open_dir(path_dir):
     list_dir_items = os.listdir(path_dir)
     list_files = ['',]
     
-    for l in list_dir_items:
-        file_name, file_extension = os.path.splitext(l)
-        if file_extension == '':
-            pass
-        elif file_extension in ACCEPTABLE_INPUT_FILE:
-            list_files.append(l)
-        else: 
-            print(loader(path_dir, file_name, file_extension))
-
-    print("Model running with the following files: " + "\n|-- ".join(list_files))
-    
     bool_confirm = False
     while not bool_confirm:
-        user_confirm = str(input("> Confirmed (Y/N)?:")).upper()
+        for l in list_dir_items:
+            file_name, file_extension = os.path.splitext(l)
+            if file_extension == '':
+                pass
+            elif file_extension in ACCEPTABLE_INPUT_FILE:
+                list_files.append(l)
+
+        print("\nModel running with the following files: " + "\n|-- ".join(list_files))
+
+        user_confirm = str(input("> Confirmed (Y/N)?: ")).upper()
         if user_confirm == 'Y':
             bool_confirm = True
-        # TODO: Put in action that user can take out files they do not want to process
+        elif user_confirm == 'N':
+            user_remove = str(input("> Which file should be removed (Enter the exact name, case sensitive)?: "))
+            list_files.remove(user_remove)
+
+            list_dir_items = list_files
+            list_files = ['',]
         
     folder_temporary = './temporary'
     check_dir(folder_temporary)
@@ -74,7 +77,6 @@ def open_dir(path_dir):
     for l in list_files:
         file_name, file_extension = os.path.splitext(l)
         dataframe = loader(path_dir, file_name, file_extension)
-        print(dataframe)
         dumper(dataframe, folder_temporary, file_name, 'PICKLE')
 
 
