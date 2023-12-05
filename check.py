cd1c3ec757e5ea1a99a490dd313b03106c80d588
YOO-uN-ee <jiyoonp0228@gmail.com>
2023-12-05 15:34:04 -0600

    resolved merge conflict

--- check.py
+++
@@ -1,9 +0,0 @@
-import pickle5 as pickle
-
-
-path = '/home/yaoyi/pyo00005/CriticalMAAS/src/umn-ta2-mineral-site-linkage/temporary/aa/site_name.pkl'
-
-with open(path, 'rb') as handle:
-    dataframe = pickle.load(handle)
-
-print(dataframe)
