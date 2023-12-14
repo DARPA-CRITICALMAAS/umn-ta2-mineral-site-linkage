e226b0185fc0ef655383539e0f330979e05c7185
YOO-uN-ee <jiyoonp0228@gmail.com>
2023-12-14 11:18:13 -0600

    updating embedding reduction

--- minelink/m3_text_based_linking/create_documents.py
+++ minelink/m3_text_based_linking/create_documents.py
@@ -19,28 +19,28 @@ def add_is(columns, df_cells, dictionary):
         
     return ''
 
-def create_documents(df_tolink, source_alias_code):
+def text_to_embedding(input_text):
+    model = AutoModel(snt_xfrmer)
+    txt_embed = model.encode(input_text, convert_to_tensor=True)
+
+    return txt_embed
+
+def create_documents(df_tolink, path_stored):
     """
     : input: df_tolink (gpd)
     : input: source_lias_code (str)
     """
     # load df_dict based on source alias code
-    df_dictionary = load_file(PATH_TMP_DIR, 'dictionary', '.pkl', additional=source_alias_code)
+    df_dictionary = load_file(path_stored, 'dictionary', '.pkl')
+
     dictionary = dict(zip(df_dictionary['label'], df_dictionary['short']))
 
     df_documentized = gpd.GeoDataFrame()
     df_documentized['unique_id'] = df_tolink.index.astype(str)
     df_documentized['geometry'] = df_tolink['geometry']
     df_documentized['document'] = pd.DataFrame(np.vectorize(add_is)(df_tolink.columns, df_tolink, dictionary)).sum(axis=1)
+    df_documentized['embedding'] = df_documentized['document'].apply(lambda x: text_to_embedding(x))
 
-    save_ckpt(df_documentized, PATH_TMP_DIR, 'df_doc', additional=source_alias_code)
-
-    df_documentized = df_tolink
+    save_ckpt(df_documentized, path_stored, 'df_doc')
 
     return df_documentized
-
-def text_to_embedding(input_text):
-    model = AutoModel(snt_xfrmer)
-    txt_embed = model.encode(input_text, convert_to_tensor=True)
-
-    return txt_embed
--- minelink/m3_text_based_linking/link_with_all.py
+++ minelink/m3_text_based_linking/link_with_all.py
@@ -1,6 +1,34 @@
+import pandas as pd
 from minelink.m2_location_based_linking.link_with_loc import link_with_loc
+from minelink.m3_text_based_linking.embedding_reduction import text_embedding_reduction
+from minelink.m3_text_based_linking.create_documents import create_documents
 
-def link_with_all():
-    link_with_loc()
-    
+def link_with_all_pl(df_tolink):
     return 0
+
+def link_with_all(df_tolink, path_stored):
+    df_loc_linked = link_with_loc(df_tolink)
+    df_Loc_linked = df_loc_linked.drop(['geometry'])
+    df_loc_linked = df_loc_linked.groupby(by='GroupID').agg(lambda x: [x])
+
+    df_loc_linked['count'] = df_loc_linked['idx'].apply(lambda x: len(x))
+    df_loc_linked_i = df_loc_linked[df_loc_linked['count'] == 1]
+    df_loc_linked_i = df_loc_linked_i.drop(['count'])    # remaining should only be groupID and index
+    df_loc_linked_i = df_loc_linked_i.explode(by='GroupID')
+
+    df_loc_linked_g = df_loc_linked[df_loc_linked['count'] != 1]
+    df_loc_linked_g = df_loc_linked_g.drop(['count'])
+    df_loc_linked_g = df_loc_linked_g.explode(by='GroupID')
+
+    # create document and do embedding reduction only on those that more than 1 count
+    # TODO: unravel the ones with count greater than 1
+    # TODO: drop document, embedding, and geometry column of evenetually all of them to concatenate
+    df_documentized = create_documents(df_loc_linked_g, path_stored)
+    df_reduced_document = text_embedding_reduction(df_documentized)
+
+
+    df_reduced_document.drop(['document', 'embedding', 'reduced_embedding'])
+
+    df_linked = pd.concat([df_loc_linked_i, df_reduced_document])
+    
+    return df_linked
