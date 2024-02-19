8666aad04b2be4912af096b51213bb17383ecbc5
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-02-19 14:24:16 -0600

    cleaning

--- __main__.py
+++ __main__.py
@@ -1,32 +1,41 @@
 from argparse import ArgumentParser
 import logging
 
-from m0_load_input.load_data import load_dir, load_mss, load_kg
-from m1_input_preprocessing.preprocess_input import preprocessing
-from m1_input_preprocessing.preprocess_schema import process_schema
+from minelink.m0_load_input.load_data import load_dir, load_kg
+from minelink.m1_input_preprocessing.preprocess_input import preprocessing
+from minelink.m1_input_preprocessing.preprocess_schema import process_schema
 
-from m2_intralinking.intralink import intralink
-from m2_intralinking.create_intra_representation import create_intra_rep
-from m3_interlinking.interlink import interlink
+from minelink.m2_intralinking.intralink import intralink
+from minelink.m2_intralinking.create_intra_representation import create_intra_rep
+from minelink.m3_interlinking.interlink import interlink
 
-from m4_postprocessing.postprocess_dataframe import postprocessing, postprocess_toGeoJSON
-from m5_save_output.save_output import *
+from minelink.m4_postprocessing.postprocess_dataframe import postprocessing, postprocess_toGeoJSON
+from minelink.m5_save_output.save_output import *
 
 def main(args):
     # logging.basicConfig(filename='minelink_run.log', format='%(levelname)s:%(message)s', level=logging.INFO)
-    list_code = load_dir(path_dir=args.data_dir, bool_dict=True)
-    mss_code = load_kg()
+    # list_code = load_dir(path_dir=args.data_dir, bool_dict=True)
+    # mss_code = load_kg()
+    # process_schema(mss_code)
 
-    preprocessing(list_code, mss_code)
+    # preprocessing(list_code, mss_code)
     
-    list_code.append(mss_code)
-    intralink(list_code, args.use_location_base)
+    # list_code = []
+    # list_code.append(mss_code)
+    # intralink(list_code, args.use_location_base)
 
-    interlink_code_copy = list_code
-    interlink(interlink_code_copy, args.use_location_base)
+    list_code = ['ah', 'ai', 'mss']
 
+    print("interlink_start")
+    interlink(list_code, args.use_location_base)
+    print("interlink done")
+
+    print("postprocess start")
     df_linked = postprocessing(bool_interlink=True)
     save_output_json(df_linked, 'output')
+    print("postprocess done")
+
+    list_code = ['ah', 'ai', 'mss']
 
     gdf_linked = postprocess_toGeoJSON(list_code, bool_interlink=True)
     save_output_geojson(gdf_linked, 'output', ['./'])
