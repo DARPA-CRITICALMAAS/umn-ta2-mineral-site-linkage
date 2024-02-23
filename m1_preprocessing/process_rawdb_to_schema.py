40fd5d4a3dbcb0c61da6f166a057ca9a0ec87ebe
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-02-23 10:52:21 -0600

    updating processing rawdb

--- m1_preprocessing/process_rawdb_to_schema.py
+++ m1_preprocessing/process_rawdb_to_schema.py
@@ -3,17 +3,21 @@ import time
 import logging
 import argparse
 
+import pickle5 as pickle
 import regex as re
 import polars as pl
-import pickle5 as pickle
+import polars.selectors as cs
+from sentence_transformers import SentenceTransformer, util
 
 from m0_loading_and_saving.load_local_data import *
 from m0_loading_and_saving.save_to_json_output import *
 
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
 def identify_id_attribute(pl_mineralsite):
     """
     Identifies the attribute (column) that is representing the unique ID of the mineral site record.
-    If there is no  unique ID, it will provide a number for the mineral site
+    If there is no unique ID, it will provide a number for the mineral site
 
     : params: pl_mineralsite
     """
@@ -40,17 +44,57 @@ def identify_id_attribute(pl_mineralsite):
 
     return False
 
-def identify_name_attribute():
+def identify_name_attribute(pl_mineralsite):
     """
+    Identifies the attribute that is representing the name and other name (i.e., name alias) of the mineral site
+    If there are no name, it will be named 'Unknown'
 
+    : params: pl_mineralsite
     """
-
     name_definition = ""
-    other_name_definition = ""
 
-    return 0
+    # Selects columns that has 'string' type attribute
+    pl_alpha_data = pl_mineralsite.select(
+        ~cs.by_dtype(pl.NUMERIC_DTYPES, pl.Boolean)
+    )
+    list_alpha_attribute = pl_alpha_data.columns
 
-def identify_geolocation_attribute():
+
+    # pl_name = pl_data.select(
+    #     pl.col(remaining_columns)
+    # ).select(
+    #     ~cs.by_dtype(pl.NUMERIC_DTYPES, pl.Boolean)
+    # )
+
+    # col_match = compare_description(dict_data, dict_target, potential_name, ['name'])
+    
+    # # Will be deleted
+    # potential = set(remaining_columns) & set(['Ftr_Name', 'Site_Name', 'site_name', 'Site'])
+    # potential = set(col_match) | potential
+    
+    # return list(potential)
+
+    # name_target = list(dict_target.keys())
+    # descrip_target = list(np.array(list(dict_target.values())).flatten())
+    # emb_target = model.encode(descrip_target, convert_to_tensor=True)
+
+    # name_against = list(dict_against.keys())
+    # descrip_against = list(np.array(list(dict_against.values())).flatten())
+    # emb_against = model.encode(descrip_against, convert_to_tensor=True)
+
+    # cosine_scores = util.cos_sim(emb_target, emb_against)
+    # cosine_scores = np.array(cosine_scores.tolist())
+
+    # idx = list(dict.fromkeys(np.where(cosine_scores > 0.45)[1]))
+
+    # col_match = np.array(name_against)[idx]
+
+    # return col_match
+
+    # name_definition = ""
+    # other_name_definition = ""
+
+def identify_geolocation_attribute(pl_mineralsite):
     """
     
     """
@@ -59,16 +103,27 @@ def identify_geolocation_attribute():
     longitude_defintion = ""
     return 0
 
-def identify_textlocation_attribute():
+def identify_textlocation_attribute(pl_mineralsite):
     """
-    
-    """
-    return 0
+    Identifies the attribute that is representing the country and/or state of the mineral site
 
-def identify_commodity_attribute():
+    : params: pl_mineralsite
+    """
+    dict_text_loc = {}
+
+    for i in list(pl_mineralsite.columns):
+        if i.lower() == 'country':
+            dict_text_loc[i] = 'country'
+        elif re.search('state', i.lower()) or re.search('province', i.lower()):
+            dict_text_loc[i] = 'state_or_province'
+
+    return dict_text_loc
+
+def identify_commodity_attribute(pl_mineralsite):
     """
     
     """
+    commodity_defintion = ""
     return 0
 
 def main(args):
