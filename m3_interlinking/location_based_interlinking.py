6acd63eb14a4484a36bc9f8ea571137bf2d61a3a
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-03-04 12:11:33 -0600

    updated gitignore

--- m3_interlinking/location_based_interlinking.py
+++ m3_interlinking/location_based_interlinking.py
@@ -14,15 +14,8 @@ def create_convex_hull_with_buffer(pl_intralinked_mineralsite):
     Creates convex hull based on geolocation of intralinked points and creates a 5km buffer around the shape
 
     : param: pl_intralinked_mineralsite = 
-    : return:
+    : return: gpd_mineralsite = geopandas dataframe of the mineralsite record with 
     """
-    # Create group name for each intralinked group
-    pl_intralinked_mineralsite = pl_intralinked_mineralsite.group_by(
-        'GroupID'
-    ).with_columns(
-        intralink_group = pl.col('source_id') + pl.lit('_g_') + pl.col('GroupID')
-    )
-
     df_intralinked_mineralsite = pl_intralinked_mineralsite.to_pandas()
     df_intralinked_mineralsite['location'] = df_intralinked_mineralsite['location'].apply(wkt.loads)
     
@@ -38,16 +31,84 @@ def create_convex_hull_with_buffer(pl_intralinked_mineralsite):
         'intra_GroupID'
     ).convex_hull.buffer(interlink_params['INTERLINK_BUFFER'])
 
-#     gpd_poly.name = 'rep_geometry'
-#     gpd_poly = gpd_poly.to_crs('WGS84').to_frame().reset_index().sort_values(by=['GroupAlias'])
-#     gpd_poly = gpd_poly.set_geometry('rep_geometry')
+    print('rename the buffer to polygon_geometry')
 
-    return 0
+    return gpd_mineralsite
 
-def compare_convex_hull_sizes(pl_mineralsite_w_convex_hull):
-    return 0
+def compare_convex_hull_sizes(gpd_mineralsite_poly1, gpd_mineralsite_poly2):
+    """
+    
+    : param: gpd_mineralsite_poly1 = geopandas dataframe 
+    : param: gpd_mineralsite_poly2 = geopandas dataframe
+    : return: pl_interlinked_mineralsite = polars dataframe
+    """
+    gpd_mineralsite_poly1 = gpd_mineralsite_poly1.rename(columns={'GroupID', 'intragroup_id1'})
+    gpd_mineralsite_poly2 = gpd_mineralsite_poly2.rename(columns={'GroupID', 'intragroup_id2'})
 
-def location_based_linking(pl_intralinked_mineralsite):
+    gpd_overlapped_mineralsite = gpd.overlay(
+        gpd_mineralsite_poly1, gpd_mineralsite_poly2,
+        how='intersection', keep_geom_type=False
+    )
+
+    try:
+        # Converting geopandas geodataframe into a polars dataframe
+        pd_overlapped_mineralsite = pd.DataFrame(gpd_overlapped_mineralsite)
+        pl_overlapped_mineralsite = pl.from_pandas(pd_overlapped_mineralsite)
+
+        # 1. Check whether the overlapping region is over the area threshold value
+        AREA_THRESHOLD = interlink_params['INTERLINK_AREA_OVERLAP']
+        pl_overlapped_mineralsite = pl_overlapped_mineralsite.filter(
+            pl.col('overlapped_area') > AREA_THRESHOLD
+        ).sort(
+            'overlapped_area'
+        )
+        
+        # 2. Leave the one with the highest overlapping region if there are multiple overlapping regions for the same record
+        pl_overlapped_mineralsite = pl_overlapped_mineralsite.unique(
+            subset = ['intragroup_id1'],
+            maintain_order=True,
+            keep='first'
+        ).unique(
+            subset = ['intragroup_id2'],
+            maintain_order=True,
+            keep='first'
+        )
+
+        # 3. Concatenate the two mineralsite records based on interlinking results
+        pd_nonoverlapped_mineralsite1 = pd.DataFrame(gpd_mineralsite_poly1)
+        pd_nonoverlapped_mineralsite2 = pd.DataFrame(gpd_mineralsite_poly2)
+        pd_nonoverlapped_mineralsite = pd.concat(
+            [pd_nonoverlapped_mineralsite1, pd_nonoverlapped_mineralsite2],
+            axis=0, ignore_index=True
+        )
+        pl_nonoverlapped_mineralsite = pl.from_pandas(pd_nonoverlapped_mineralsite)
+
+        pl_interlinked_mineralsite = pl.concat(
+            [pl_overlapped_mineralsite, pl_nonoverlapped_mineralsite],
+            how='vertical'
+        ).drop('area')
+
+        del (pd_overlapped_mineralsite, pl_overlapped_mineralsite, 
+             pd_nonoverlapped_mineralsite, pd_nonoverlapped_mineralsite1, pd_nonoverlapped_mineralsite2)
+
+        return pl_interlinked_mineralsite
+
+    except:
+        # Case where there is no overlapping region possible between the two geopandas dataframe
+        # Just concatenate the two input geopandas dataframe
+        df_mineralsite1 = pd.DataFrame(gpd_mineralsite_poly1)
+        df_mineralsite2 = pd.DataFrame(gpd_mineralsite_poly2)
+
+        pd_interlinked_mineralsite = pd.concat(
+            [df_mineralsite1, df_mineralsite2],
+            axis=0, ignore_index=True
+        )
+
+        del (df_mineralsite1, df_mineralsite2)
+
+        return pl.from_pandas(pd_interlinked_mineralsite)
+
+def location_based_linking(list_intralinked_mineralsite):
     
 
     return 0
