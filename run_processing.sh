1903ab4d00ae9ce7a8c3c251ad95c23d1f9477be
Jiyoon Pyo <jiyoonp@gmail.com>
2024-11-14 14:17:13 -0600

    processing shell script path redirection

--- run_processing.sh
+++ run_processing.sh
@@ -43,7 +43,7 @@ fi
 
 # Create new GitHub Branch for pushing in new data
 echo "Creating branch $github_branch in minmod data repository"
-cd ../ta2-minmod-data
+cd ta2-minmod-data
 git checkout main
 git pull
 git checkout -b $github_branch
