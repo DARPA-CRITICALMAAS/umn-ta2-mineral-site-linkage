67e4e8f615687739f12211236beb7479f9c1a450
Jiyoon Pyo <jiyoonp0228@gmail.com>
2025-01-09 16:56:56 -0600

    append other evaluation data

--- data_drag.py
+++ data_drag.py
@@ -1,25 +1,13 @@
-# from utils.load_kg_data import *
-
-# for commodity in ['tungsten', 'lithium']:
-#     pl_data = load_minmod_kg(commodity=commodity)
-#     pl_data.write_csv(f'/home/yaoyi/pyo00005/Spatial_Entity_Matching/data/raw/{commodity}_all.csv')
-
 import polars as pl
 import pandas as pd
 from itertools import combinations
 
-pl_data = pl.read_csv('/home/yaoyi/pyo00005/CriticalMAAS/umn-ta2-mineral-site-linkage/_archive_resource/GBW_MRDS.csv', infer_schema_length=0).select(
-    pl.col(['OBJECTID', 'COMB_DEP_IDs'])
-).with_columns(
-    pl.col('COMB_DEP_IDs').str.split('|')
-).explode('COMB_DEP_IDs').with_columns(
-    pl.col('COMB_DEP_IDs').str.strip_chars()
-).with_columns(
-    ms_uri = pl.lit('site__mrdata-usgs-gov-mrds__') + pl.col('COMB_DEP_IDs')
+pl_data = pl.read_csv('/home/yaoyi/pyo00005/Spatial_Entity_Matching/data_gt/0raw/nickel.csv').with_columns(
+    pl.col('ms_uri').str.replace('https://minmod.isi.edu/resource/', '')
 )
 
 list_uris = pl_data['ms_uri'].to_list()
-list_groups = pl_data['OBJECTID'].to_list()
+list_groups = pl_data['group'].to_list()
 
 dict_grouping = dict(zip(list_uris, list_groups))
 
@@ -32,6 +20,6 @@ pl_data = pl.from_pandas(df).with_columns(
     pl.when(pl.col('uri1_grp') == pl.col('uri2_grp')).then(1).otherwise(0).alias('ground_truth')
 ).drop(['uri1_grp', 'uri2_grp'])
 
-pl_data.write_csv('/home/yaoyi/pyo00005/CriticalMAAS/umn-ta2-mineral-site-linkage/tests/data/w_gbw.csv')
+pl_data.write_csv('/home/yaoyi/pyo00005/CriticalMAAS/umn-ta2-mineral-site-linkage/tests/data/ni_umidwest.csv')
 
-print(pl_data)
+# print(pl_data)
